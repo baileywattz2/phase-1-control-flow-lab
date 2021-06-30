e7baf390cee4d3c38d35907501bc8ece3b7cdcdb
bailey watkins <bailey.watkins@rocketmail.com>
2021-06-30 14:48:04 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance){
+  let greeting 
+
+  if(distance<=400){
+    greeting='This one is on me!';
+  }else if(distance>2000 && distance<2500){
+    greeting='I will gladly take your thirty bucks.';
+  }else if(distance>2500){
+    greeting='No can do.';
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city==='NYC' ? 'Ok, sounds good.': 'No go.'
+   }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+   function switchOnCharmFromTip(tip){
+    switch(tip){
+      case 'generous':
+        return('Thank you so much.');
+        break;
+      case 'not as generous':
+        return('Thank you.');
+        break;
+      default:
+      case 'Anything else':
+        return('Bye.')
+        break;
+  
+    }
+  }
