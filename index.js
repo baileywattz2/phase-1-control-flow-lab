9c38563f0ddd44c4fe4d86f2739d047608d71017
bailey watkins <bailey.watkins@rocketmail.com>
2021-10-05 17:57:36 -0400

    completed assignment

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ function ternaryCheckCity(city){
 
 
 
-   function switchOnCharmFromTip(tip){
-    switch(tip){
-      case 'generous':
-        return('Thank you so much.');
-        break;
-      case 'not as generous':
-        return('Thank you.');
-        break;
-      default:
-      case 'Anything else':
-        return('Bye.')
-        break;
-  
-    }
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return('Thank you so much.');
+      break;
+    case 'not as generous':
+      return('Thank you.');
+      break;
+    default:
+    case 'Anything else':
+      return('Bye.')
+      break;
+
   }
+} 
